1914c029f77d1ea67a407697b7f5d7d42e26b159
rgm94 <rgmartos1994@gmail.com>
2026-02-26 17:09:44 +0100

    funcionalidades para editar los productos

--- crud-frontend/src/app/components/ProductForm.tsx
+++ crud-frontend/src/app/components/ProductForm.tsx
@@ -15,16 +15,26 @@
 "use client";
 import { useForm } from "react-hook-form";
 import client from "@/src/lib/axios";
-import { FormValues } from "@/src/types/index";
+import { FormValues, ProductFormProps } from "@/src/types/index";
 
-export default function ProductForm({ onAdded }: { onAdded: () => void }) {
+export default function ProductForm({ onAdded, initialData, isEditing = false }: ProductFormProps) {
     //Inicializamos el Hook useForm
-    const { register, handleSubmit, reset, formState: { errors } } = useForm<FormValues>();
+    const { register, handleSubmit, reset, formState: { errors } } = useForm<FormValues>({
+        //Rellena el formulario si tiene datos
+        defaultValues: initialData || { nombre: '', precio: 0 }
+    });
 
     //Ejecutamos la función si los datos pasan la validación
     const onSubmit = async (data: FormValues) => {
         try {
-            await client.post("/productos", data);
+
+            if (isEditing && initialData) {
+                //usamos put porque creamos un producto
+                await client.put(`/productos/${initialData.id}`, data);
+            } else {
+                //usamos Post porque creamos un producto
+                await client.post("/productos", data);
+            }
             reset();
             onAdded();
         } catch (error) {
@@ -61,7 +71,7 @@ export default function ProductForm({ onAdded }: { onAdded: () => void }) {
                 </div>
 
                 <button type="submit" className="w-full md:w-auto bg-blue-600 hover:bg-blue-700 text-white font-bold py-3 px-8 rounded-xl transition-all">
-                    Crear
+                    {isEditing ? "Guardar Cambios" : "Crear Producto"}
                 </button>
             </form>
         </div>
